6799f561726f8f2daa3ecfe4259be9c3cfdf3f65
Snochowski Mikołaj <snochopl@gmail.com>
2021-01-12 22:08:17 +0100

    Implement more advanced url check

--- src/fragments/isPinkBikeUrl.ts
+++ src/fragments/isPinkBikeUrl.ts
@@ -1,15 +1,18 @@
 const isPinkBikeUrl = (url: string) => {
-  if (!url.includes('pinkbike')) {
-    return false;
-  }
-  if (!url.includes('/photo/')) {
-    return false;
-  }
-  if (!url.includes('https')) {
-    return false;
-  }
+  if (!url.includes('pinkbike')) return false;
+  if (!url.includes('/photo/')) return false;
+  if (!url.includes('https')) return false;
 
-  return true;
+  const routes = url.split('/');
+  if (!routes.length) return false;
+  const photoIndex = routes.findIndex((value: string) => value === 'photo');
+
+  if (!photoIndex && photoIndex !== 0) return false;
+  const photoId = routes[photoIndex];
+  if (!photoId) return false;
+  if (!photoId.match(/^[0-9]*$/)) return false;
+
+  return photoId.length === 8;
 }
 
 export default isPinkBikeUrl;
