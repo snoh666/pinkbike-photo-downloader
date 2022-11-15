fa3b39c3f1c326cbf8c13f330fb8b662940ca677
Mikolaj <snochopl@gmail.com>
2022-11-15 20:30:42 +0100

    refactor: use main component instead of straightn feature

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,13 @@ import { ThemeProvider } from 'styled-components';
 
 import theme from '@styles/theme';
 import GlobalStyles from '@styles/GlobalStyles';
-import useDownloadHandler from '@hooks/useDownloadHandler';
+import Main from '@pages/Main';
 
 const App: React.FC = () => {
-  const { download, error } = useDownloadHandler();
-
-  const handleDownload = () => download();
-
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <button onClick={handleDownload} type='button'>
-        Download current iamge
-      </button>
-      {error || <p>{error}</p>}
+      <Main />
     </ThemeProvider>
   );
 };
