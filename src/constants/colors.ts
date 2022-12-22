1aedd8d57c14afd143ceed30678be4b430bd416d
Mikolaj <snochopl@gmail.com>
2022-12-22 17:36:05 +0100

    feat: add in new colors pallete

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,16 +1,9 @@
 const colors = {
-  brightBlue: '#0062ff',
-  blueish: '#1162FB',
-  white: '#ffffff',
-  dark: '#2e3c4b',
-  darkThree: '#272a3f',
-  battleshipGrey: '#74797e',
-  grey: '#E5E5E5',
-  black10: '#0000001a',
-  black08: '#00000008',
-  whiteSeven: '#F4F7FA',
-  red: '#C10C0C',
-  appleGreen: '#53AC12',
+  black: '#0F0905',
+  redDarker: '#CC444B',
+  red: '#CC444B',
+  redLight: '#DF7373',
+  white: '#F1EDEE',
 };
 
 export default colors;
