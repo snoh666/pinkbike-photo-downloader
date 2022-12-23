c84c305ce499532292542e83468eabb981f18094
Mikolaj <snochopl@gmail.com>
2022-12-23 16:56:38 +0100

    feat: add inside background with roundness on layout

--- src/layouts/Basic/index.tsx
+++ src/layouts/Basic/index.tsx
@@ -4,7 +4,9 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   min-width: 300px;
   min-height: 300px;
-  background: ${({ theme }) => theme.colors.black10};
+  background: ${({ theme }) => theme.colors.black};
+  border-radius: 8px;
+  overflow: hidden;
 `;
 
 const Basic: React.FC = ({ children }) => {
