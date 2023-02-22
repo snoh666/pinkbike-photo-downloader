144f6bde0033a3d1697ed7257512633dc4079a19
Mikolaj <snochopl@gmail.com>
2023-02-22 22:38:14 +0100

    feat: include footer component

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,14 @@
 import Basic from '@layouts/Basic';
 import DownloadContent from '@components/DownloadContent';
 import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 const Main = () => {
   return (
     <Basic>
       <Header />
       <DownloadContent />
+      <Footer />
     </Basic>
   );
 };
