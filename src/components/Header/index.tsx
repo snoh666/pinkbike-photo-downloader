cbd9feec2d5212e98ea5d1412fba515c49546f15
Mikolaj <snochopl@gmail.com>
2023-02-22 22:05:13 +0100

    refactor: improve heading content

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,26 @@
-import { ReactNode } from 'react';
 import styled from 'styled-components';
 
-const Heading = styled.h1``;
+const Flex = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  margin: 1rem 0.25rem;
+`;
 
-const Header = (): ReactNode => (
-  <Heading>Click to download highest resolution image</Heading>
+const Heading = styled.h1`
+  font-size: 1rem;
+`;
+
+const SubHeading = styled.h4`
+  font-size: 0.75rem;
+`;
+
+const Header = () => (
+  <Flex>
+    <Heading>PinkBike Image Download</Heading>
+    <SubHeading>Download highest resolution image available</SubHeading>
+  </Flex>
 );
 
 export default Header;
