18e3c9b6c6bdf5d0dda159f8e224e461bfb5be38
Mikolaj <snochopl@gmail.com>
2023-02-22 22:05:53 +0100

    feat: include back header component

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,9 +1,11 @@
 import Basic from '@layouts/Basic';
 import DownloadContent from '@components/DownloadContent';
+import Header from '@components/Header';
 
 const Main = () => {
   return (
     <Basic>
+      <Header />
       <DownloadContent />
     </Basic>
   );
