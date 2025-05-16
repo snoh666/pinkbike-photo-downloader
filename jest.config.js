ecaa6e354f0d19eb5faeab3554c7801a0773aca5
Mikolaj <snochopl@gmail.com>
2025-05-16 15:35:54 +0200

    test(jest): add working tests file

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    testPathIgnorePatterns: ['/node_modules/', '/cache/'],
-    roots: ['<rootDir>/src'],
-    setupFilesAfterEnv: ['<rootDir>/jest/setupTests.js'],
-    transformIgnorePatterns: [
-        'node_modules/(?!(react-grid-layout|react-resizable|react-syntax-highlighter))/',
-    ],
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/cache/'],
+  roots: ['<rootDir>/src'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-grid-layout|react-resizable|react-syntax-highlighter))/',
+  ],
 };
