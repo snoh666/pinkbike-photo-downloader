6c2da8d846c94be9ef171be4dc725d2f2b3ceec5
Mikolaj <snochopl@gmail.com>
2025-05-16 15:10:07 +0200

    chore(testing): move from enzyme to RTL

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,6 @@ module.exports = {
     testPathIgnorePatterns: ['/node_modules/', '/cache/'],
     roots: ['<rootDir>/src'],
     setupFilesAfterEnv: ['<rootDir>/jest/setupTests.js'],
-    snapshotSerializers: ['enzyme-to-json/serializer'],
     transformIgnorePatterns: [
         'node_modules/(?!(react-grid-layout|react-resizable|react-syntax-highlighter))/',
     ],
--- jest/setupTests.js
+++ jest/setupTests.js
@@ -1,4 +1,4 @@
-require('@testing-library/jest-dom');
+import '@testing-library/jest-dom';
 
 const enzyme = require('enzyme');
 
