871dbc430c0bc067ba02c5e5b310f5dbd1a5a2dc
Mikolaj <snochopl@gmail.com>
2025-09-13 23:45:50 +0200

    chore: add mocks for query within chrome tabs

--- src/mocks/mock-extension-apis.ts
+++ src/mocks/mock-extension-apis.ts
@@ -1,5 +1,6 @@
 global.chrome = {
   tabs: {
+    query: async () => { throw new Error("Unimplemented.") },
     create: async () => { throw new Error("Unimplemented.") }
   },
 } as unknown as typeof chrome;
