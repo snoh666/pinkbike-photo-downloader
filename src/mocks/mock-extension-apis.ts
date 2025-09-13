93749cb82079869902fc9cf2ee09211ea2935bc9
Mikolaj <snochopl@gmail.com>
2025-09-14 00:17:40 +0200

    chore: ran prettier format through mock extension api

--- src/mocks/mock-extension-apis.ts
+++ src/mocks/mock-extension-apis.ts
@@ -1,6 +1,10 @@
 global.chrome = {
   tabs: {
-    query: async () => { throw new Error("Unimplemented.") },
-    create: async () => { throw new Error("Unimplemented.") }
+    query: async () => {
+      throw new Error('Unimplemented.');
+    },
+    create: async () => {
+      throw new Error('Unimplemented.');
+    },
   },
 } as unknown as typeof chrome;
