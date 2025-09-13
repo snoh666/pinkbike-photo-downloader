ccba5a91cd94308eba85df35dce2dc146d2b5b6f
Mikolaj <snochopl@gmail.com>
2025-09-13 22:59:15 +0200

    chore: make tests run again

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/cache/'],
@@ -7,7 +7,4 @@ module.exports = {
     '^src/(.*)$': '<rootDir>/src/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-grid-layout|react-resizable|react-syntax-highlighter))/',
-  ],
 };
