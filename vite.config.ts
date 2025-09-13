1a781a913f0a1944ff5c2556275edbe2c38b2ec3
Mikolaj <snochopl@gmail.com>
2025-09-13 22:32:31 +0200

    chore: add vite build process

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+      output: {
+        inlineDynamicImports: true,
+      }
+    },
+  },
+});
